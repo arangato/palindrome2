e36aceaed41a2c94b3b300721bec1477a1418937
Maxim Grabarnik <maxim@lightricks.com>
2022-02-18 21:59:16 +0200

    measure number of loop iterations in a second

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # palindrom2
 
 A description of this package.
+
+a simple loop perform 1733902255 iterations a second.
+
+
--- Sources/palindrom2/main.swift
+++ Sources/palindrom2/main.swift
@@ -1,3 +1,14 @@
 import CoreFoundation
 import Foundation
 print("Hello, world!")
+
+let step: UInt64 = 1_000_000_000
+let start = Date()
+print(UInt64.max / step)
+for i in 0..<UInt64.max {
+  if (i % step) == 0 {
+    let ellapsedSeconds = Date().timeIntervalSince(start)
+    let rate = Double(i) / ellapsedSeconds
+    print("\(ellapsedSeconds): \(i),   rate = \(round(rate)) / sec")
+  }
+}

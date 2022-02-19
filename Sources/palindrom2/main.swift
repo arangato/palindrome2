9cf2eaa24bdbdfa9e308541eafe9de569c6ba765
Maxim Grabarnik <maxim@lightricks.com>
2022-02-19 15:40:54 +0200

    measure naive isBinaryPalindrom

--- Sources/palindrom2/main.swift
+++ Sources/palindrom2/main.swift
@@ -1,3 +1,17 @@
 import CoreFoundation
 import Foundation
 print("Hello, world!")
+
+
+let step: Int64 = 100_000_000
+let start = Date()
+print(Int64.max / step)
+var count = 0
+for i in Int64(Int32.max)..<Int64.max {
+  count += isBinaryPalindrom(i) ? 1 : 0
+  if (i % step) == 0 {
+    let ellapsedSeconds = Date().timeIntervalSince(start)
+    let rate = Double(i) / ellapsedSeconds
+    print("\(ellapsedSeconds): \(i),   rate = \(round(rate)) / sec  \(count)")
+  }
+}

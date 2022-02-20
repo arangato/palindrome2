48e57c9f39ca276668acef6af745c766392c9674
Maxim Grabarnik <maxim@lightricks.com>
2022-02-20 23:53:46 +0200

    measure

--- Sources/palindrom2/main.swift
+++ Sources/palindrom2/main.swift
@@ -1,6 +1,17 @@
 import CoreFoundation
 import Foundation
-print("Hello, world!")
 
-makePalindrom(mirrorDigits: 1)
-makePalindrom(mirrorDigits: 2)
+let step: Int64 = 10_000_000
+let start = Date()
+print(Int64.max / step)
+var count = 0
+for i in Int64(Int32.max)..<Int64.max {
+  let decimalPalindrome = "1\(i)1"
+  let n = Int64(decimalPalindrome)!
+  count += Binary.isPalindromLookup(n) ? 1 : 0
+  if (i % step) == 0 {
+    let ellapsedSeconds = Date().timeIntervalSince(start)
+    let rate = Double(i - Int64(Int32.max)) / ellapsedSeconds
+    print("\(ellapsedSeconds): \(i),   rate = \(round(rate)) / sec  \(count)")
+  }
+}

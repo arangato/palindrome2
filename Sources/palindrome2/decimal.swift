635c1e691d95560f47e92429bff6dd4d71036cb8
Maxim Grabarnik <maxim@lightricks.com>
2022-02-21 12:47:57 +0200

    measure E2E rate using long decimal palindromes.

--- Sources/palindrome2/decimal.swift
+++ Sources/palindrome2/decimal.swift
@@ -13,13 +13,26 @@ func makePalindrome(halfDigitsCount: Int) {
     start *= 10
   }
 
+  let step: Int64 = 1_000_000
+  let startTime = Date()
+  var count = Int64(0)
+  var found = 0
+
   let format = "%0\(halfDigitsCount)d"
   // no middle digit
   for m in start...upperEnd {
     let high = String(format: format, m)
     let low = String(high.reversed())
     let p = "\(high)\(low)"
-    print(p)
+    
+    count += 1
+    let i = Int64(p)!
+    found += Binary.isPalindromeLookup(i) ? 1 : 0
+    if (count % step) == 0 {
+      let ellapsedSeconds = Date().timeIntervalSince(startTime)
+      let rate = Double(count) / ellapsedSeconds
+      print("\(ellapsedSeconds): \(i),   rate = \(round(rate)) / sec  \(found)")
+    }
   }
 
   for m in start...upperEnd {
@@ -27,7 +40,15 @@ func makePalindrome(halfDigitsCount: Int) {
       let high = String(format: format, m)
       let low = String(high.reversed())
       let p = "\(high)\(middle)\(low)"
-      print(p)
+
+      count += 1
+      let i = Int64(p)!
+      found += Binary.isPalindromeLookup(i) ? 1 : 0
+      if (count % step) == 0 {
+        let ellapsedSeconds = Date().timeIntervalSince(startTime)
+        let rate = Double(count) / ellapsedSeconds
+        print("\(ellapsedSeconds): \(i),   rate = \(round(rate)) / sec  \(found)")
+      }
     }
   }
 }
--- Sources/palindrome2/main.swift
+++ Sources/palindrome2/main.swift
@@ -1,11 +1,4 @@
 import CoreFoundation
 import Foundation
 
-makePalindrome(halfDigitsCount: 1)
-print("----------")
-makePalindrome(halfDigitsCount: 2)
-print("----------")
-makePalindrome(halfDigitsCount: 3)
-print("----------")
-makePalindrome(halfDigitsCount: 4)
-print("----------")
+makePalindrome(halfDigitsCount: 8)

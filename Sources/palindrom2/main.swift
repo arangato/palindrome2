d55af5ce6ab620b6feae25e2f35790faa5f866a0
Maxim Grabarnik <maxim@lightricks.com>
2022-02-21 06:57:02 +0200

    use nibbles for decimal digits

--- Sources/palindrom2/main.swift
+++ Sources/palindrom2/main.swift
@@ -1,17 +1,17 @@
 import CoreFoundation
 import Foundation
 
-let step: Int64 = 10_000_000
-let start = Date()
-print(Int64.max / step)
-var count = 0
-for i in Int64(Int32.max)..<Int64.max {
-  let decimalPalindrome = "1\(i)1"
-  let n = Int64(decimalPalindrome)!
-  count += Binary.isPalindromLookup(n) ? 1 : 0
-  if (i % step) == 0 {
-    let ellapsedSeconds = Date().timeIntervalSince(start)
-    let rate = Double(i - Int64(Int32.max)) / ellapsedSeconds
-    print("\(ellapsedSeconds): \(i),   rate = \(round(rate)) / sec  \(count)")
+func printDecimal(_ n: UInt16) {
+  var s = ""
+  var nn = n
+  for _ in 0...3 {
+    s += String(nn >> 12 - 6)
+    nn <<= 4
   }
+
+  print(String(format:"%04X", n) + ": " + s)
+}
+
+for i in UInt16(0)...UInt16(9000) {
+  printDecimal((0x6666 + i) | 0x6666)
 }
